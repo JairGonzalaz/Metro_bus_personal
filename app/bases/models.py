e9daa42541027fcf8efa06cfb5058e801ed833c9
jairgonzalaz <jairgonzalaz@gmail.com>
2023-05-19 17:05:52 -0600

    ultimo commit

--- app/bases/models.py
+++ app/bases/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class ClaseModelo(models.Model):
+    estado=models.BooleanField(default=True)#estado activo/inactivo
+    fc = models.DateTimeField(auto_now_add=True)#fecha creacion 
+    fm = models.DateTimeField(auto_now=True)# fecha modificacion
+    uc = models.ForeignKey(User,on_delete=models.CASCADE)#usuario crea
+    um = models.IntegerField(blank=True,null=True)#usuario que esta logeado modifica
+
+    class Meta:
+        abstract=True
+
+
